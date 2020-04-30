d1ca3f671dd728f4600b1f0dcde9c793f4fc89d0
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-04-30 16:45:20 -0300

    Exercicio Blackjack (sem desafio completo)

---
+++ semana3/blackjack/desafio.js
@@ -0,0 +1,19 @@
+// EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+
+//const carta = comprarCarta(); // Sorteia uma carta.Porexemplo, o rei de ouros
+//8.
+/*if (primeiraCartaU === A && segundaCartaU === A){
+   console.log("Reinicie o jogo!")
+}*/
+
+
+//9.
+confirm(`Suas cartas são ${primeiraCartaU.texto} ${segundaCartaU.texto}. A carta revelada do computador é ${primeiraCartaC.texto}." + "\n" + "Deseja comprar mais uma carta?`)
+}
+
+   
+
+
+
+//console.log(carta.texto) // imprime o texto da carta.Exemplo:"K♦️" (indica "K" de ouros)
+//console.log(carta.valor) // imprime o valor da carta(umnúmero). Exemplo: 10 (dado que "K" vale 10)
