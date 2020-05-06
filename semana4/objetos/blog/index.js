c2064d05139fcad831438256a0551b8cd265fd49
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-05-06 16:30:18 -0300

    Exercicio Blog (incompleto)

---
+++ semana4/objetos/blog/index.js
@@ -0,0 +1,5 @@
+    let postTitulo  = document.getElementById('input-titulo').value
+    let postAutor   = document.getElementById('input-autor').value
+    let postTexto   = document.getElementById('areaDoPost').value
+
+console.log("ola mundo")
