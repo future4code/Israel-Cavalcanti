07f57c876410cc8852cf1b152dbac094e55f9298
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-05-07 13:55:01 -0300

    Exercicio Blog finalizado

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,5 +1,31 @@
-    let postTitulo  = document.getElementById('input-titulo').value
-    let postAutor   = document.getElementById('input-autor').value
-    let postTexto   = document.getElementById('areaDoPost').value
 
-console.log("ola mundo")
+let array =[]
+
+function botaoClique(){
+
+  event.preventDefault()
+
+  let postTitulo  = document.getElementById('input-titulo').value
+  let postAutor   = document.getElementById('input-autor').value
+  let postTexto   = document.getElementById('areaDoPost').value
+
+  const objeto = {
+    titulo: postTitulo,
+    autor: postAutor,
+    texto: postTexto
+  }
+
+  document.getElementById('input-titulo').value = ""
+  document.getElementById('input-autor').value  = ""
+  document.getElementById('areaDoPost').value   = ""
+
+  array.push(objeto)
+    
+  console.log(objeto)
+
+  postaDiv(objeto)
+}
+
+function postaDiv(objeto){
+    document.getElementById('areaDaPostagem').innerHTML += `<h2>${objeto.titulo}</h2> <p>${objeto.autor}</p> <p>${objeto.texto}</p>`
+}
