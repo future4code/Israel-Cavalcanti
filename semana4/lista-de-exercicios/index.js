aced400030abfa1f9810dc3b40a1df258ac5ce84
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-05-08 12:24:00 -0300

    Ex interpretação de código - Ex. 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+// EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO
+/*1. O código possui uma função 'conversorDeMoeda' onde o usuário insere um número referente á cotação do dólar em um prompt na tela, esse valor é guardado na variável 'cotacao'. 
+A função possui um 'return'que retornará o 'R$' e valor de 'cotacao' multiplicado pela variavel 'valorEmDolar' que é inserida na funcão ao ser invocada pela variável 'meuDinheiro'.
+No caso, o valor inserido nessa variável é 100. Por fim é impresso no console o valor da variavel 'meuDinheiro': 100 * (valor a ser inserido pelo usuário).
+*/
