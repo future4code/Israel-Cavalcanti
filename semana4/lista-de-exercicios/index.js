84b3be0d8add8de5cec31926690c1ae5ce228270
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-05-08 12:44:39 -0300

    Ex interpretação de código - Ex. 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,16 @@
 A função possui um 'return'que retornará o 'R$' e valor de 'cotacao' multiplicado pela variavel 'valorEmDolar' que é inserida na funcão ao ser invocada pela variável 'meuDinheiro'.
 No caso, o valor inserido nessa variável é 100. Por fim é impresso no console o valor da variavel 'meuDinheiro': 100 * (valor a ser inserido pelo usuário).
 */
+
+
+/*2.O código possui uma função 'investeDinheiro', dentro dela, uma variável 'valorAposInvestimento' e um 'switch' tipoDeInvestimento' onde estão listados algumas opções de tipo de investimento com um valor predeterminado para cada caso. 
+Se o usuário informar um tipo diferente das opções listadas, um alert é acionado informando "Tipo de investimento informado incorreto!".
+A função retorna com o return o valor da variável 'valorAposInvestimento'.
+Fora da função há duas variáveis que invocam a função acima:
+I. 'novoMontante' busca dentro do switch a palavra "Ações" e insere o número 150. "Ações" é encontrado e aplica o número inserido em 'valor': 150 * 1.1. O resultado é retornado no return: 165.
+II. 'segundoMontante' busca dentro do switch a "Tesouro Direto" e insere o número 200. "Tesouro Direto" não é encontrado e retorna o alert.
+
+No console é impresso:
+165
+TIPO DE INVESITMENTO INFORMADO INCORRETO!
+*/
