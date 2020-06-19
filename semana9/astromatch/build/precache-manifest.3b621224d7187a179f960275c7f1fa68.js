2f3b8c1e8d5dee80689ea178184ab3860b5978fc
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-06-19 16:52:52 -0300

    Projeto Astromatch primeira entrega

---
+++ semana9/astromatch/build/precache-manifest.3b621224d7187a179f960275c7f1fa68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95f45472ad25148e5c684dd28178592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7387add433c5f52f7b0a",
+    "url": "/static/css/main.75403d27.chunk.css"
+  },
+  {
+    "revision": "6561ea2d5685143a3b5e",
+    "url": "/static/js/2.da3da3a0.chunk.js"
+  },
+  {
+    "revision": "89d3385f7d39af76bd84ec1c3fe43df2",
+    "url": "/static/js/2.da3da3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7387add433c5f52f7b0a",
+    "url": "/static/js/main.a0d8713c.chunk.js"
+  },
+  {
+    "revision": "61c1bc095b57d4b4904d",
+    "url": "/static/js/runtime-main.3588f4a7.js"
+  }
+]);
