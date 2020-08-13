25826e3a1bdfccec2146d41c19856d1db3bb2022
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-08-13 17:03:15 -0300

    Ajustes de endpoints que não estavam funcionando

--- semana17/knex-express/src/index.ts
+++ semana17/knex-express/src/index.ts
@@ -40,6 +40,7 @@ const getActorById = async (id: string): Promise<any> => {
     SELECT * FROM Actor WHERE id = '${id}'
     `);
     console.log(result[0][0]);
+    return result[0][0];
   } catch (error) {
     console.log(error);
   }
@@ -48,6 +49,7 @@ const getActorById = async (id: string): Promise<any> => {
 // getActorById("001");
 
 // EX 1.b
+// FUNÇÃO QUE RECEBE UM NOME E RETORNA UM ATOR COM ESSE NOME:
 const getActorByName = async (name: string): Promise<any> => {
   try {
     const result = await connection.raw(`
@@ -62,6 +64,7 @@ const getActorByName = async (name: string): Promise<any> => {
 // getActorByName("Ingrid Guimarães");
 
 // EX 1.c
+// FUNÇÃO QUE RECEBE UM GÊNERO PARA BUSCAR TODOS ATORES DESSE GÊNERO:
 const getActorByGender = async (gender: string): Promise<any> => {
   try {
     const result = await connection.raw(`
@@ -81,6 +84,10 @@ const getActorByGender = async (gender: string): Promise<any> => {
 
 //getActorByGender("female");
 
+//===============================================================
+
+// EX 2
+// FUNÇÃO PARA CRIAR UM ATOR:
 const createActor = async (
   id: string,
   name: string,
@@ -100,6 +107,7 @@ const createActor = async (
 };
 
 // EX 2.a
+// FUNÇÃO QUE RECEBE ID E SALÁRIO PARA ATUALIZAR O SALÁRIO DO ATOR DESSE ID COM O SALÁRIO INDICADO:
 const updateSalaryByIdAndSalary = async (
   id: string,
   salary: number
@@ -119,9 +127,10 @@ const updateSalaryByIdAndSalary = async (
 //updateSalaryByIdAndSalary("004", 230000);
 
 // EX 2.b
+// FUNÇÃO QUE RECEBE ID PARA DELETAR O ATOR DESSE ID:
 const deleteActorById = async (id: string): Promise<any> => {
   try {
-    await connection("Actor").delete().where({ id });
+    await connection("Actor").where({ id }).del();
     console.log("Ator deletado com sucesso!");
   } catch (error) {
     console.log(error);
@@ -131,12 +140,12 @@ const deleteActorById = async (id: string): Promise<any> => {
 //deleteActorById("005");
 
 // EX 2.c
+// FUNÇÃO QUE RECEBE UM GÊNERO E RETORNA A MÉDIA SALARIAL DESSE GÊNERO:
 const getAvgByGender = async (gender: string): Promise<any> => {
   try {
     const result = await connection("Actor")
       .avg("salary AS Média salarial")
       .where({ gender });
-
     console.log(result[0]);
   } catch (error) {
     console.log(error);
@@ -146,11 +155,14 @@ const getAvgByGender = async (gender: string): Promise<any> => {
 //getAvgByGender("female");
 //getAvgByGender("male");
 
+//===============================================================
+
 // EX 3
+// ENDPOINT (MÉTODO) PARA BUSCAR ATOR PELO ID - Path Param (passa direto na URL)
 app.get("/actor/:id", async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
-    const actor = await getActorByName(id);
+    const actor = await getActorById(id);
 
     res.status(200).send(actor);
   } catch (error) {
@@ -159,8 +171,10 @@ app.get("/actor/:id", async (req: Request, res: Response) => {
     });
   }
 });
+// Exemplo: Passar na URL > 3000/actor/012
 
 // EX 3.c
+// ENDPOINT (MÉTODO) PARA BUSCAR ATOR PELO GÊNERO - Query Param
 app.get("/actor", async (req: Request, res: Response) => {
   try {
     const count = await getActorByGender(req.query.gender as string);
@@ -174,9 +188,17 @@ app.get("/actor", async (req: Request, res: Response) => {
     });
   }
 });
+//FUNCIONOU NO POSTMAN:
+// QUERY PARAMS Exemplos:
+/*
+1- Ou passa direto na URL > 3000/actor?gender=female
+2- Ou adiciona no Query Params KEY = gender & Value = female / male
+*/
+
+//===============================================================
 
 // EX 4
-
+// ENDPOINT (MÉTODO) PARA CRIAR UM NOVO ATOR - Body
 app.put("/actor", async (req: Request, res: Response) => {
   try {
     await createActor(
@@ -184,20 +206,32 @@ app.put("/actor", async (req: Request, res: Response) => {
       req.body.name,
       req.body.salary,
       new Date(req.body.dateOfBirth),
-      req.body.salary
+      req.body.gender
     );
-    res.status(200).send();
+    res.status(200).send({ message: "Actor criado com sucesso!" });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
+// FUNCIONOU NO POSTMAN:
+// BODY - RAW - JSON: Exemplo:
+/*
+{
+    "id": "013",
+    "name": "Testando o PUT sem 15 (2)",
+    "salary": "15",
+    "dateOfBirth": "2020-02-22",
+    "gender": "female"
+}
+*/
 
 // EX 4.a
+// ENDPOINT (MÉTODO) QUE RECEBE ID E SALÁRIO PARA ATUALIZAR SALÁRIO - Body
 app.post("/actor", async (req: Request, res: Response) => {
   try {
     await updateSalaryByIdAndSalary(req.body.id, req.body.salary);
     res.status(200).send({
-      message: "Success POST",
+      message: "Salário atualizado com sucesso!",
     });
   } catch (error) {
     res.status(400).send({
@@ -205,24 +239,41 @@ app.post("/actor", async (req: Request, res: Response) => {
     });
   }
 });
+// FUNCIONOU NO POSTMAN:
+// BODY - RAW - JSON: Exemplo:
+/*
+{
+  "id": "013",
+  "salary": "15", 
+}
+*/
 
 //EX 4.b
+// ENDPOINT (MÉTODO) QUE RECEBE ID PARA DELETAR ATOR - Path Param (passa direto na URL)
 app.delete("/actor/:id", async (req: Request, res: Response) => {
   try {
     await deleteActorById(req.params.id);
+    res.status(200).send({
+      message: "Ator deletado com sucesso!",
+    });
   } catch (error) {
     res.status(400).send({
       message: error.message,
     });
   }
 });
+// Exemplo: Passar na URL > 3000/actor/012
+
+//===============================================================
 
 // EX 5
+// FUNÇÃO QUE CRIA UM NOVO FILME:
 const createMovie = async (
   id: string,
   title: string,
   synopsis: string,
   releaseDate: Date,
+  rating: number,
   playingLimitDate: Date
 ) => {
   await connection
@@ -231,22 +282,25 @@ const createMovie = async (
       title,
       synopsis,
       release_date: releaseDate,
+      rating,
       playing_limit_date: playingLimitDate,
     })
-    .into("Movie");
+    .into("Movies");
 };
 
+// ENDPOINT (MÉTODO) PARA CRIAR UM NOVO FILME - Body
 app.post("/movie", async (req: Request, res: Response) => {
   try {
     await createMovie(
       req.body.id,
       req.body.title,
       req.body.synopsis,
-      req.body.releaseDate,
-      req.body.playingLimitDate
+      req.body.release_date,
+      req.body.rating,
+      req.body.playing_limit_date
     );
     res.status(200).send({
-      message: "Sucess POST",
+      message: "Filme criado com sucesso!",
     });
   } catch (error) {
     res.status(400).send({
@@ -254,3 +308,15 @@ app.post("/movie", async (req: Request, res: Response) => {
     });
   }
 });
+// FUNCIONOU NO POSTMAN:
+// BODY - RAW - JSON: Exemplo:
+/*
+{
+  "id": "013",
+  "title": "Novo filme 2020", 
+  "synopsis": "Um filme ", 
+  "release_date": "2015-05-05",
+  "rating": "5", 
+  "playing_limit_date": "2020-05-05"
+}
+*/
