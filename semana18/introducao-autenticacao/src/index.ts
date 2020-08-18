01a1722d0d87c78a92f7090521b236e824ca7702
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-08-18 15:31:53 -0300

    Correção de código - Ex.1 e 2 funcionando

--- semana18/introducao-autenticacao/src/index.ts
+++ semana18/introducao-autenticacao/src/index.ts
@@ -14,7 +14,7 @@ export const connection = knex({
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
+    database: process.env.DB_NAME,
   },
 });
 
